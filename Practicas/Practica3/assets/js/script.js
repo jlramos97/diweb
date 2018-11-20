99a4c0273204931dc924477987206e28c795a813
José Luis <jlramos97@gmail.com>
2018-11-20 01:12:03 +0100

    Practicamente responsive todo para pc y tablet

--- Practicas/Practica3/assets/js/script.js
+++ Practicas/Practica3/assets/js/script.js
@@ -3,4 +3,26 @@
     window.addEventListener("load", function(e) {
         document.getElementById("video").play();
     });
+
+    var pulsado = false;
+
+    document.getElementById("botonHeader").addEventListener("click", function(e) {
+        e.preventDefault();
+        
+        if(!pulsado) {
+
+        }
+        
+        document.getElementById("headerElement1").style.display = "block";
+        
+        
+        document.getElementById("headerElement2").style.display = "block";
+
+
+        document.getElementById("headerElement3").style.display = "block";
+
+
+        document.getElementById("headerElement4").style.display = "block";
+    });
+
 })();
