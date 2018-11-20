6426a0954dec3959e24cf98c0acbfb49284bcbd7
José Luis <jlramos97@gmail.com>
2018-11-20 14:49:04 +0100

    Contacto practicamente 100% responsive

--- Practicas/Practica3/assets/js/script.js
+++ Practicas/Practica3/assets/js/script.js
@@ -1,28 +1,9 @@
 (function() {
 
-    window.addEventListener("load", function(e) {
-        document.getElementById("video").play();
-    });
-
-    var pulsado = false;
-
-    document.getElementById("botonHeader").addEventListener("click", function(e) {
-        e.preventDefault();
-        
-        if(!pulsado) {
-
-        }
-        
-        document.getElementById("headerElement1").style.display = "block";
-        
-        
-        document.getElementById("headerElement2").style.display = "block";
-
-
-        document.getElementById("headerElement3").style.display = "block";
-
-
-        document.getElementById("headerElement4").style.display = "block";
-    });
+    if (document.getElementById("video") !== null) {
+        window.addEventListener("load", function(e) {
+            document.getElementById("video").play();
+        });
+    }
 
 })();
